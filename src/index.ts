021d5dfc8a806bf776dcf1ea11922aae49de4901
anmolsah <kumarxxx242424@gmail.com>
2025-10-17 23:53:35 +0530

    Update server log message to include full URL

--- src/index.ts
+++ src/index.ts
@@ -25,5 +25,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });

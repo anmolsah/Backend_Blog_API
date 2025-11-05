bbe0afadbd119d4c479a1cd677aced3e9291c364
anmolsah <kumarxxx242424@gmail.com>
2025-11-05 22:39:33 +0530

    feat: add createBlog controller in blogControllers

---
+++ src/controllers/blogControllers.ts
@@ -0,0 +1,8 @@
+import type {Response} from express;
+import Blog from "../models/Blog";
+import type { AuthRequest } from '../middlewares/auth';
+
+
+export async function createBlog(req:AuthRequest,res:Response){
+    
+}

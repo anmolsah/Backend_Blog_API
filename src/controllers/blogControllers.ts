67be9361896428b9015358c6824fca9fbf26993e
anmolsah <kumarxxx242424@gmail.com>
2025-11-06 22:12:09 +0530

    feat: add endpoint to fetch a single blog by id

--- src/controllers/blogControllers.ts
+++ src/controllers/blogControllers.ts
@@ -29,3 +29,17 @@ export async function listBlogs(req:AuthRequest,res:Response){
     }
 }
 
+//get blog by if
+export async function getBlog(req:AuthRequest,res:Response){
+    try {
+        const {id} =req.params as {id:string};
+        const blog = await Blog.findById(id).populate("author","name email");
+        if(!blog){
+            return res.status(404).json({message:"Blog not found"});
+        }
+        return res.json(blog);
+    } catch (error) {
+        return res.status(500).json({message:"Internal server error"});
+        
+    }
+}
